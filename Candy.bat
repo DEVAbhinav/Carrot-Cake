2c91fca0e758211a6d67dbd52d0345945260bdab
Devabhinav <abhinavpandey.1996@gmail.com>
2016-12-14 23:24:21 +0530

    Minor update candy version 1

--- Candy.bat
+++ Candy.bat
@@ -6,22 +6,28 @@ for /f "tokens=* delims=" %%p in ('mshta.exe "%~f0"') do (
     set "choice=%%p"
 )
 if %choice%==codeChef (
-explorer D:\Abhinav\Projects\carrot-cake\CodeChef
-@rem d:
-@rem cd D:\Abhinav\Projects\carrot-cake\CodeChef
-start cmd.exe D:\Abhinav\Projects\carrot-cake\CodeChef
+explorer D:\Abhinav\Projects\3rd yr\carrot-cake\CodeChef
+cd D:\Abhinav\Projects\3rd yr\carrot-cake\CodeChef
+d:
+start cmd.exe 
+subl D:\Abhinav\Projects\3rd yr\carrot-cake\CodeChef
 )
 
 if %choice%==fantasy (
 explorer D:\Abhinav\Projects\3rd yr\fantasy
-
-start cmd.exe D:\Abhinav\Projects\3rd yr\fantasy
+cd D:\Abhinav\Projects\3rd yr\fantasy
+cd d:
+cmd.exe 
+subl D:\Abhinav\Projects\3rd yr\fantasy
 )
 
 if %choice%==scrapeAmazon (
 explorer D:\Abhinav\Projects\3rd yr\scrapeAmazon
-
+cd D:\Abhinav\Projects\3rd yr\scrapeAmazon
+d:
 start cmd.exe D:\Abhinav\Projects\3rd yr\scrapeAmazon
+subl D:\Abhinav\Projects\3rd yr\scrapeAmazon
+
 )
 
 echo your choice is %choice%
