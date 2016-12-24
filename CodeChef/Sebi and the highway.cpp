173fbd2ab2c0d3ff7d4d142dce6087f1ad196c99
Devabhinav <abhinavpandey.1996@gmail.com>
2016-12-24 20:34:17 +0530

    Sebi way successful

--- CodeChef/Sebi and the highway.cpp
+++ CodeChef/Sebi and the highway.cpp
@@ -14,7 +14,7 @@ int main()
 	int s[T],sg[T],fg[T],d[T],t[T],i,Answer[T];
 	
 	//taking input of the data.
-	char outputChoice[3][] = { "SEBI","FATHER","DRAW"};
+	char outputChoice[3][8] = { "SEBI","FATHER","DRAW"};
 	
 	while(i<T) {
 		cin>>s[i]>>sg[i]>>fg[i]>>d[i]>>t[i];
@@ -40,11 +40,11 @@ int main()
 int correctGuess (int s,int sg,int fg,int d, int t) {
 	
 	//calculating actual Speed.
-	float actualSpeed = s+(float)d*50/t;
+	float actualSpeed = s+(float)d*180/t;
 	
-	if(abs((actualSpeed-sg) > abs(actualSpeed-fg)))
+	if(abs(actualSpeed-sg) > abs(actualSpeed-fg))
 		return 1;
-	else if(abs((actualSpeed-sg) < abs(actualSpeed-fg)))
+	else if(abs(actualSpeed-sg) < abs(actualSpeed-fg))
 		return 0;
 	else return 2;
 
