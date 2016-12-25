85047e3ce84f2acf738871076cf070aac7018704
Devabhinav <abhinavpandey.1996@gmail.com>
2016-12-25 13:01:07 +0530

    IN C++

--- CodeChef/Entrance Exam.cpp
+++ CodeChef/Entrance Exam.cpp
@@ -4,30 +4,16 @@ using namespace std;
 
 
 
-//fctrl is a recursive function for calculating number of zeroz in factorial.
-//int fctrl (long int numberToBeFactorial, int powerOf5)
+//is a c++ program for codechef External Exam problem.
+//Run this in codechef g++ 14.something compiler.
+
 
 long int minMarksNeeded (/*long int , long int */);
 
 int main() {
-	int t;
-	//input number of test Case.
-	cin>>t;
-	int i=0; 
-	long int inputArr[4],result[t+1]; 
-	
-	//taking input
-	for(inputArr[4] = {0};i<t;) {
-		cin>>inputArr[i++];	
-	}
+	int T; //test case
 
-	//calling factorial for each iput
-	for (i=0;i<t;i++) {
-		result[i] = fctrl(inputArr[i],5);
+	while(i<T) {
+		cin>>
 	}
-
-	//printing result
-	for (i=0;i<t;i++)
-		cout<<result[i]<<endl;
-	return 0;
 }
